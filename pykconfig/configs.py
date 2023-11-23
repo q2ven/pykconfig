27be80b3c746f1142712cd031d25fbff22e0679b
Kuniyuki Iwashima <kuniyu@amazon.com>
2023-11-23 17:42:02 +0000

    configs: Add Configs class.
    We can create Configs instance by feeding .config file.
    
      >>> from configs import Configs
      >>>
      >>> x86_64 = Configs('config-x86_64')
      >>> len(x86_64)
      2483
      >>> print(x86_64)
      CONFIG_CC_VERSION_TEXT="gcc (GCC) 11.3.1 20221121 (Red Hat 11.3.1-4)"
      CONFIG_CC_IS_GCC=y
      CONFIG_GCC_VERSION=110301
    
    Note Configs instance holds each kernel configs in the same order that
    they show up in .config file.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

---
+++ pykconfig/configs.py
@@ -0,0 +1,44 @@
+import re
+from collections import OrderedDict
+
+
+class Configs(object):
+    regex = re.compile(r'^CONFIG_(.*?)=(.*?)\n')
+
+    def __init__(self, filename=""):
+        self.configs = OrderedDict()
+
+        self.filename = filename
+        self.parse()
+
+    def __len__(self):
+        return len(self.configs)
+
+    def __iter__(self):
+        return iter(self.configs)
+
+    def __getitem__(self, key):
+        return self.configs[key]
+
+    def __str__(self):
+        config_list = []
+
+        for key in self:
+            config_list.append(f'CONFIG_{key}={self[key]}')
+
+        return '\n'.join(config_list)
+
+    def parse(self):
+        if not self.filename:
+            return self
+
+        with open(self.filename, 'r') as f:
+            for config in f.readlines():
+                match = self.regex.match(config)
+
+                if not match:
+                    continue
+
+                self.configs[match.group(1)] = match.group(2)
+
+        return self
